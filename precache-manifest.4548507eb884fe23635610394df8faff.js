9c86187bcb372f8c75b2be87e037cef70148700e
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-01-08 22:29:51 +0530

    Updates

---
+++ precache-manifest.4548507eb884fe23635610394df8faff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bece15b0bca4949ab82aec0d75df56",
+    "url": "/topgear/index.html"
+  },
+  {
+    "revision": "a3d822529f998f624c8b",
+    "url": "/topgear/static/css/main.faf622c7.chunk.css"
+  },
+  {
+    "revision": "4f6cca9b40e7ca94ee55",
+    "url": "/topgear/static/js/2.f652fc9f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/topgear/static/js/2.f652fc9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3d822529f998f624c8b",
+    "url": "/topgear/static/js/main.e3ad79d5.chunk.js"
+  },
+  {
+    "revision": "04b07f00106edd00c1f6",
+    "url": "/topgear/static/js/runtime-main.597f142b.js"
+  }
+]);
